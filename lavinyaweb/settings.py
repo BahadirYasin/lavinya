bb38fe5013a197661f17c762007829a2017ed02f
BahadirYasin <bahadir.cetinsoy2000@gmail.com>
2025-07-14 12:23:06 +0300

    updated settings.py

--- lavinyaweb/settings.py
+++ lavinyaweb/settings.py
@@ -28,6 +28,12 @@ DEBUG = True
 
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
 
+CSRF_TRUSTED_ORIGINS = [
+    'https://lavinya-production.up.railway.app',
+    'http://127.0.0.1:8000',
+    'http://localhost:8000',
+]
+
 
 # Application definition
 
