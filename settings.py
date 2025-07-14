c8a04f133c39fffd96b44520bd1c19913c1ad8d0
BahadirYasin <bahadir.cetinsoy2000@gmail.com>
2025-07-14 09:58:23 +0300

    updated settings.py

--- settings.py
+++ settings.py
@@ -16,11 +16,8 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY from environment
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "insecure-default-key")
+DEBUG = os.environ.get("DEBUG", "False") == "True"
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split(",")
 
 
