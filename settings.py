653bb2d4033faaa27daf22b5efed11294ee223f8
BahadirYasin <bahadir.cetinsoy2000@gmail.com>
2025-07-14 12:13:13 +0300

    updated settings.py

--- settings.py
+++ settings.py
@@ -18,7 +18,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "insecure-default-key")
 DEBUG = os.environ.get("DEBUG", "False") == "True"
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split(",")
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
+
 
 CSRF_TRUSTED_ORIGINS = [
     'https://lavinya-production.up.railway.app',
