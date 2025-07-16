9a8c255175be160017b27bf7d23745712f554e09
BahadirYasin <bahadir.cetinsoy2000@gmail.com>
2025-07-16 13:34:35 +0300

    Switched from postre to sqlite

--- lavinyaweb/settings.py
+++ lavinyaweb/settings.py
@@ -87,9 +87,10 @@ WSGI_APPLICATION = 'lavinyaweb.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default='postgresql://postgres:RwLwJVeSNvrWJAynvUMZoAjOciXHlPGF@postgres.railway.internal:5432/railway'
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
