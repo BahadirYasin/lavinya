a62e1ec2eacced68503442e4ce0d30509921ca43
BahadirYasin <bahadir.cetinsoy2000@gmail.com>
2025-07-17 14:24:12 +0300

    added sitemaps.xml

--- lavinyaweb/urls.py
+++ lavinyaweb/urls.py
@@ -19,9 +19,18 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from django.contrib import sitemaps
+from django.contrib.sitemaps.views import sitemap
+from main.sitemaps import StaticViewSitemap
+
+sitemaps_dict = {
+    'static': StaticViewSitemap,
+}
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),  
+    path('sitemap.xml', sitemap, {'sitemaps': sitemaps_dict}, name='django.contrib.sitemaps.views.sitemap'),
 ]
 
 if settings.DEBUG:
