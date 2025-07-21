8e31ace9435cdb511f597eea262ee1e53c7d01cb
BahadirYasin <bahadir.cetinsoy2000@gmail.com>
2025-07-21 12:25:36 +0300

    added cloudinary

--- lavinyaweb/settings.py
+++ lavinyaweb/settings.py
@@ -52,6 +52,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cloudinary',
     'cloudinary_storage',
     'main',
     'django.contrib.sitemaps',
