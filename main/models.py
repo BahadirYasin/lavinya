1ba306cba3359755386c102b43c3f4bf4f357f2a
BahadirYasin <bahadir.cetinsoy2000@gmail.com>
2025-07-21 14:23:52 +0300

    added models.py cloudinaryfield

--- main/models.py
+++ main/models.py
@@ -46,7 +46,7 @@ class MenuItem(models.Model):
     description = models.TextField()
     ingredients = models.TextField("İçindekiler", blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    image = models.ImageField(upload_to='menu_items/')
+    image =  CloudinaryField('image')
 
     def __str__(self):
         section_title = getattr(self.section, "title", "")
@@ -56,7 +56,7 @@ class MenuItem(models.Model):
 
 class GalleryImage(models.Model):
     title = models.CharField(max_length=100, blank=True)
-    image = models.ImageField(upload_to='gallery/')
+    image = CloudinaryField('image')
     uploaded_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
@@ -85,7 +85,7 @@ class CafeItem(models.Model):
     description = models.TextField(blank=True)
     ingredients = models.TextField(blank=True, null=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    image = models.ImageField(upload_to='cafe_menu/')
+    image =  CloudinaryField('image')
 
     def __str__(self):
         return self.name
