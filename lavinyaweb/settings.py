91f9763681134b951c0bd8b7ab71447bb2ff4894
BahadirYasin <bahadir.cetinsoy2000@gmail.com>
2025-07-21 13:45:15 +0300

    changed cloudinary settings

--- lavinyaweb/settings.py
+++ lavinyaweb/settings.py
@@ -150,9 +150,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Cloudinary Media Settings
 CLOUDINARY_STORAGE = {
-    'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME'),
-    'API_KEY': os.environ.get('CLOUDINARY_API_KEY'),
-    'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET')
+    'CLOUD_NAME': 'dleullkdl',
+    'API_KEY': '793892168798272',
+    'API_SECRET': 'C-SIoLDUhIFa9GwasvOpXycJj9w'
 }
 
 # Cloudinary her ortamda aktif olsun
